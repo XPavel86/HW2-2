00fe7a90e8ade4a22edd293cd9e01d0002d7cdc2
Pavel <rezusx@mail.ru>
2024-02-13 01:31:34 +0300

    Code end

--- HW2-2/ViewController.swift
+++ HW2-2/ViewController.swift
@@ -9,9 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var red: UIView!
-    @IBOutlet var green: UIView!
-    @IBOutlet var yellow: UIView!
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var yellowLight: UIView!
     
     @IBOutlet var changeСolorButton: UIButton!
     
@@ -21,17 +21,39 @@ class ViewController: UIViewController {
         
         changeСolorButton.layer.cornerRadius = 10
         
-        [red, green, yellow].forEach( { item in
+        [redLight, greenLight, yellowLight].forEach( { item in
             item.alpha = 0.3
             item.layer.cornerRadius = item.layer.frame.width / 2.0
         } )
         
     }
     
+    var pressCount = 0
+    
     // вылетает ошибка: changeСolorButtonDidTapped(changeСolorButton)
     @IBAction func changeСolorButtonDidTapped(_ sender: UIButton) {
         
         sender.setTitle("NEXT", for: .normal)
+        
+        pressCount += 1
+        
+        let trafficLights: (_, _, _) -> () = {
+            self.redLight.alpha = $0
+            self.yellowLight.alpha = $1
+            self.greenLight.alpha = $2
+        }
+        
+        switch pressCount {
+        case 1: trafficLights(1, 0.3, 0.3)
+        case 2: trafficLights(0.3, 1, 0.3)
+        case 3: trafficLights(0.3, 0.3, 1)
+
+        default:
+            pressCount = 0
+            trafficLights(0.3, 0.3, 0.3)
+            sender.setTitle("START", for: .normal)
+        }
+        
     }
     
     }
